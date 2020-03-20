675b7f17bfb7535bc4faf2e7d7357c463b5b3696
Andrew Warren-Love <andrew@awarrenlove.com>
2020-03-19 20:49:43 -0400

    Wrap benchmark discounts in table-container

--- src/app/src/components/benchmark/DiscountTable.tsx
+++ src/app/src/components/benchmark/DiscountTable.tsx
@@ -8,30 +8,32 @@ const DiscountTable = (props: { method: string, totals: any[], saveTargetDiscoun
                 {props.method}
             </div>
             <div className="panel-block">
-                <table className="table is-fullwidth is-striped is-hoverable">
-                    <thead>
-                        <tr>
-                            <th className="has-text-centered">Weight</th>
-                            <th className="has-text-centered">Count</th>
-                            <th className="has-text-centered" colSpan={2}>Grace Discount</th>
-                            <th className="has-text-centered" colSpan={2}>Discount</th>
-                            <th className="has-text-centered" colSpan={2}>Earned Discount</th>
-                            <th className="has-text-centered" colSpan={2}>Performance Pricing</th>
-                            <th className="has-text-centered" colSpan={2}>Automation Discount</th>
-                            <th className="has-text-centered" colSpan={2}>Total Discount</th>
-                            <th className="has-text-centered">Net Spend</th>
-                            <th className="has-text-centered">Net Discount</th>
-                            <th className="has-text-centered">Proposed Discount</th>
-                            <th className="has-text-centered">Proposed Net Spend</th>
-                            <th className="has-text-centered">Net-Net Comparison</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.totals && props.totals.length && props.totals.map((total: any) => (
-                            <DiscountRow total={total} saveTargetDiscount={props.saveTargetDiscount} />
-                        ))}
-                    </tbody>
-                </table>
+                <div className="table-container">
+                    <table className="table is-striped is-hoverable">
+                        <thead>
+                            <tr>
+                                <th className="has-text-centered">Weight</th>
+                                <th className="has-text-centered">Count</th>
+                                <th className="has-text-centered" colSpan={2}>Grace Discount</th>
+                                <th className="has-text-centered" colSpan={2}>Discount</th>
+                                <th className="has-text-centered" colSpan={2}>Earned Discount</th>
+                                <th className="has-text-centered" colSpan={2}>Performance Pricing</th>
+                                <th className="has-text-centered" colSpan={2}>Automation Discount</th>
+                                <th className="has-text-centered" colSpan={2}>Total Discount</th>
+                                <th className="has-text-centered">Net Spend</th>
+                                <th className="has-text-centered">Net Discount</th>
+                                <th className="has-text-centered">Proposed Discount</th>
+                                <th className="has-text-centered">Proposed Net Spend</th>
+                                <th className="has-text-centered">Net-Net Comparison</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {props.totals && props.totals.length && props.totals.map((total: any) => (
+                                <DiscountRow total={total} saveTargetDiscount={props.saveTargetDiscount} />
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
